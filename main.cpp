289d45848bc72f27cfe846607cf53584b771f3cc
Ricardo <rickisierra919@gmail.com>
2023-10-10 20:00:55 -0600

    Version 2

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 //
 // File: main.cpp
 // Author: Ricardo Sierra Roa - A01709887
-// Date: 06/10/2023
+// Date: 10/10/2023
 //
 // =================================================================
 
@@ -11,44 +11,68 @@
 
 using namespace std;
 
+
+// =================================================================
+// Funcion para determinar sobreviviente y posteriormente posicion 
+// donde se debe colocar para sobrevivir dada n personas y k numero 
+// de personas para realizar conteo
+//
+// @param n numero de peronas y k numero de personas
+// @return posicion de inicio segura
+// @Complexity	O(n^2)
+// =================================================================
 int survivor(int n, int k) {
     list<int> survivors;
     list<int>::iterator it;
+    int aux, position;
 
     for (int i = 1; i <= n; i++){
-        survivors.push_back(i); //crear fila con la cantidad de personas
+        survivors.push_back(i);
     }
 
-    survivors.push_front(survivors.back()); 
-    survivors.pop_back();
+    while (survivors.size() > 1) { //@Complexity O(n^2)
 
-    while (survivors.size() > 1) {
-
-        //Funcion para eliminar elemento
-        for (int i = 1; i < k+1; i++) {
-            survivors.push_back(survivors.front()); //mandar el elemento del frente al final
-            survivors.pop_front(); //eliminar el elemento del frente 
+        //Recorrer lista y eliminar elemento k
+        for (int i = 1; i < k; i++) {
+            survivors.push_back(survivors.front());
+            survivors.pop_front();
         }
-        survivors.pop_front(); //eliminar el elemento del frente
+        survivors.pop_front();
 
-        //Funcion para recorrer elemento a la posicion de elemento eliminado
-        it = survivors.begin(); //Declarar iterador para recorrer la lista
-        advance(it, (k-1)); //Avanzar el iterador hasta llegar a la posicon deseada
-        survivors.splice(survivors.begin(), survivors, it); //Utilizar el metodo splice para mover el elemento al inicio
+        //Encontrar elemento k que se reacomodara
+        for (int i = 1; i < k; i++) {
+            survivors.push_back(survivors.front()); 
+            survivors.pop_front();
+        }
         
-        for (const int& elemento : survivors) {
-            cout << elemento << " ";
+        //Guardar elemento que se reacomodara en una variable
+        aux = survivors.front();
+        survivors.pop_front();
+        
+        //Reacomodar lista para continuar eliminacion posterior
+        for (int i = 1; i < k; i++) {
+            survivors.push_front(survivors.back()); 
+            survivors.pop_back();
         }
-        cout << endl;
-
+        
+        //Insertar elemento k en la posicion del elemento previamente eliminado
+        survivors.push_back(aux);
     }
 
-    return survivors.front(); //regresar el sobreviviente
+    //Calcular posicion de inicio segura
+    position = (n - survivors.front()) + 2;
+    
+    //Casos especiales
+    if(survivors.front()==1){
+        return 1;
+    }else{
+        return position;
+    }
 }
 
-int main() {
+int main(){
     int n, k;
     cin >> n >> k;
-    cout << survivor(n, k) << endl;
+    cout << survivor(n, k); //@Complexity O(n^2)
     return 0;
 }
